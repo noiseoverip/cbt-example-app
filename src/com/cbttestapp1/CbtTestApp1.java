80794b17cda98a20555c4edd559eec507b696669
saulius <noiseoverip@gmail.com>
2013-06-06 01:18:13 +0200

    add auto-generate buttons

--- src/com/cbttestapp1/CbtTestApp1.java
+++ src/com/cbttestapp1/CbtTestApp1.java
@@ -4,45 +4,72 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.LinearLayout.LayoutParams;
 
 import com.testapp1.R;
 
 /**
  * Extremely simple Activity that displays two buttons. Once one of them is
- * clicked, another Activity takes place and show a message related the
- * clicked button.
+ * clicked, another Activity takes place and show a message related the clicked
+ * button.
  * 
  * @author SauliusAlisauskas
  * 
  */
 public class CbtTestApp1 extends Activity {
+	private static final int numberOfButtonPairs = 2;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		final Intent intent = new Intent(this, AfterClick.class);
-
-		findViewById(R.id.Button01).setOnClickListener(
-				new View.OnClickListener() {
-
-					@Override
-					public void onClick(View v) {
-						intent.putExtra("message", "From button 1");
-						startActivity(intent);
-
-					}
-				});
-
-		findViewById(R.id.Button02).setOnClickListener(
-				new View.OnClickListener() {
-
-					@Override
-					public void onClick(View v) {
-						intent.putExtra("message", "From button 2");
-						startActivity(intent);
-					}
-				});
-
+		
+		LayoutParams buttonLayoutParams = new LinearLayout.LayoutParams(
+				LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT, 1.0f);
+		LayoutParams buttonPairContainerParams = new LinearLayout.LayoutParams(
+				LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT, 0);
+		
+		for (int i = 0; i < numberOfButtonPairs; i++) {
+			LinearLayout linearLayout = new LinearLayout(this);
+			linearLayout.setOrientation(LinearLayout.HORIZONTAL);
+			linearLayout.setLayoutParams(buttonPairContainerParams);
+			
+			Button button1 = new Button(this);
+			button1.setLayoutParams(buttonLayoutParams);
+			String text1 = "button" + (i*2 + 1);
+			button1.setText(text1);
+			button1.setContentDescription(text1);
+			button1.setOnClickListener(new ButtonClickListener(text1, this));
+			
+			Button button2 = new Button(this);
+			button2.setLayoutParams(buttonLayoutParams);
+			String text2 = "button" + (i*2 + 2);
+			button2.setText(text2);
+			button2.setContentDescription(text2);
+			button2.setOnClickListener(new ButtonClickListener(text2, this));
+			
+			linearLayout.addView(button1);
+			linearLayout.addView(button2);
+			
+			((LinearLayout)this.findViewById(R.id.container)).addView(linearLayout);
+		}
+	}
+	
+	class ButtonClickListener implements View.OnClickListener {
+		private String text;		
+		
+		public ButtonClickListener(String text, Activity activity) {
+			this.text = text;
+		}
+		
+		@Override
+		public void onClick(View v) {
+			Intent intent = new Intent(v.getContext(), AfterClick.class);
+			intent.putExtra("message", text);
+			startActivity(intent);			
+		}
+		
 	}
 }
